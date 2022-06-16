75488e0329b1b4b365a6b7ff3f83f48ebb416bc5
isub897 <ivsubritzky@gmail.com>
2022-06-16 16:55:06 +1200

    uploaded simple navbar

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Navbar from './components/Navbar/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+
+      <Navbar />
+      {/* <Navbar />
+      <TitleArea />
+      <Collections /> */}
+    </div>
+  );
+}
+
+export default App;
+
+// color palette
+// https://palettes.shecodes.io/palettes/102
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,21 @@
+import React from "react";
+import './Navbar.css'
+
+const Navbar = () => {
+    return(
+        <nav class="topnav shadow-5">
+            <div className="logo">
+                {/* <a class="active" href="#home">Home</a> */}
+                <img className="ba" alt="logo" src="" />
+            </div>
+            <div className="routes">
+                <a href="#news">News</a> 
+                <a href="#charts">Charts</a>
+                <a href="#projects">Projects</a>
+            </div>
+        </nav>
+
+    )
+}
+
+export default Navbar;
