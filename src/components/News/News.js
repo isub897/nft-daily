19a9cb8082fec65e219ae7e7c7433ab9bda308f5
isub897 <ivsubritzky@gmail.com>
2022-06-22 12:34:48 +1200

    news skeleton upload

--- src/components/News/News.js
+++ src/components/News/News.js
@@ -1,19 +1,37 @@
 import React from "react";
 import ImageSlider from "./ImageSlider/ImageSlider";
+import StoryList from "./StoryList/StoryList";
 
 class News extends React.Component {
     constructor() {
         super();
         this.state = {
+            storyData: [],
+            dataLoaded: false
         }
     }
 
+    componentDidMount() {
+        fetch('http://localhost:3000/news')
+        .then(response => response.json())
+        .then(data => {
+            this.setState({storyData: data});
+            this.setState({dataLoaded: true});
+        })
+    }
+
     render() {
+
         return(
             <div className="news">
-                <ImageSlider />
-                {/* <ImageSlider />
-                <Strories /> */}
+                {this.state.dataLoaded
+                    ?<div>
+                        <ImageSlider storyData={this.state.storyData}/>
+                        <StoryList storyData={this.state.storyData}/>
+                    </div>
+                    :<div>Loading...</div>
+                }
+                
             </div>
         )
     }
