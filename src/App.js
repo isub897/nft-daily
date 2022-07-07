f477a5a0d68027fdd8233212f861c7cfde4c91a3
isub897 <ivsubritzky@gmail.com>
2022-07-07 13:31:37 +1200

    basic signin route

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import Navbar from './components/Navbar/Navbar';
 import Charts from './components/Charts/Charts';
 import News from './components/News/News';
 import About from './components/About/About';
+import Signin from './components/Signin/Signin'
 import React from 'react';
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      route: "charts"
+      route: "signin"
     }
   }
 
@@ -44,7 +45,10 @@ class App extends React.Component {
         ?<News />
         :(this.state.route === "charts"
           ?<Charts />
-          :<About />
+          :(this.state.route === "about"
+            ?<About />
+            :<Signin />
+          )
         )
       }
       </div>
@@ -57,3 +61,10 @@ export default App;
 
 // color palette
 // https://palettes.shecodes.io/palettes/102
+
+
+//sign in
+// setup a 1min session from server-side
+
+// setup a signedin state that shows the name and dashboard and info instead of signin on navbar and dashboard
+// have this state backed by a cookie in the backend so taht when localhost:3000 is called, isAuth will be triggered and then signedin state will be presented on the front-end with all the user information from the previous session (something like that)
