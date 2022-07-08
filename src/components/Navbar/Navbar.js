f04d6edc8a6d505e31ac47a1d7ba861d9586e196
isub897 <ivsubritzky@gmail.com>
2022-07-08 17:54:55 +1200

    navbar routing

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,7 +3,7 @@ import './Navbar.css'
 import dropdownImage from './Images/dropdown.webp';
 import logo from './Images/logo.png';
 
-const Navbar = ({onSignedin, signedin, onDropdownChange, onRouteChange}) => {
+const Navbar = ({route, onSignedin, signedin, onDropdownChange, onRouteChange}) => {
 
     const onSignOut = () => {
         onRouteChange("signin");
@@ -18,7 +18,11 @@ const Navbar = ({onSignedin, signedin, onDropdownChange, onRouteChange}) => {
         <nav className="topnav shadow-5">
             <div className="logo">
                 <img 
-                onClick={() => onRouteChange("dashboard")}
+                onClick={signedin
+                            ? () => onRouteChange("dashboard")
+                            : () => onRouteChange("signin")
+                    
+                    }
                 alt="logo" 
                 src={logo}/>
             </div>
@@ -26,16 +30,31 @@ const Navbar = ({onSignedin, signedin, onDropdownChange, onRouteChange}) => {
                 {signedin
                     ?<a  onClick={onSignOut}
                      href="#news">Sign Out</a>
-                    :<a  onClick={()=> onRouteChange("signin")}
-                     href="#news">Sign In</a>  
+                    :(route === "signin"
+                        ?<a className="selected" onClick={()=> onRouteChange("signin")}
+                          href="#news">Sign In</a>
+                        :<a  onClick={()=> onRouteChange("signin")}
+                          href="#news">Sign In</a>  
+                    )
                 }
- 
-                <a  onClick={()=> onRouteChange("news")}
-                    href="#news">News</a> 
-                <a  onClick={()=> onRouteChange("charts")}
-                    href="#charts">Charts</a>
-                <a  onClick={()=> onRouteChange("about")} 
-                href="#projects">About</a>
+                {route === "news"
+                    ?<a className="selected" onClick={()=> onRouteChange("news")}
+                      href="#news">News</a> 
+                    :<a  onClick={()=> onRouteChange("news")}
+                      href="#news">News</a> 
+                }
+                {route === "charts"
+                    ?<a className="selected" onClick={()=> onRouteChange("charts")}
+                      href="#charts">Charts</a>
+                    :<a  onClick={()=> onRouteChange("charts")}
+                      href="#charts">Charts</a>
+                }
+                {route === "about"
+                    ?<a className="selected" onClick={()=> onRouteChange("about")} 
+                      href="#projects">About</a>
+                    :<a  onClick={()=> onRouteChange("about")} 
+                      href="#projects">About</a>
+                }
                 <div className="dropdown-nav">
                     <button 
                         className="dropbtn-nav"
@@ -44,8 +63,12 @@ const Navbar = ({onSignedin, signedin, onDropdownChange, onRouteChange}) => {
                         <i className="fa fa-caret-down"></i>
                     </button>
                     <div id="myDropdown-nav" className="dropdown-content-nav">
-                        <a  onClick={()=> onRouteChange("signin")}
-                                href="#news">Sign In</a> 
+                        {signedin
+                            ?<a  onClick={onSignOut}
+                                href="#news">Sign Out</a>
+                            :<a  onClick={()=> onRouteChange("signin")}
+                              href="#news">Sign In</a>
+                        } 
                         <a  onClick={()=> onRouteChange("news")}
                             href="#news">News</a> 
                         <a  onClick={()=> onRouteChange("charts")}
